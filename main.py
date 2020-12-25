f097ae1ac321595975f88503747fc741e3af4805
thisaraMJ <madushantjg@gmail.com>
2020-12-26 01:42:16 +0530

    working bot

--- main.py
+++ main.py
@@ -1,16 +1,53 @@
-# This is a sample Python script.
+import smtplib  #simple mail transfer protocol library
+import speech_recognition as sr
+import pyttsx3
+from email.message import EmailMessage
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+listener = sr.Recognizer()
+talker = pyttsx3.init()
+
+email_list = {"thisara": "madushantjg@gmail.com"}
+
+def talk(text):
+    talker.say(text)
+    talker.runAndWait()
+
+def speech_to_text():
+        try:
+            with sr.Microphone() as source:
+                print('Listening..')
+                voice = listener.listen(source)
+                message = listener.recognize_google(voice)
+                print(message)
+                return message.lower()
+        except:
+            pass
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def get_mail_info():
+    talk("give me the receiver's email address")
+    name = speech_to_text()
+    address = email_list["name"]
+
+    talk("give me the subject of email")
+    subject = speech_to_text()
+
+    talk("give me the message")
+    content = speech_to_text()
+
+    sendEmail(address, subject, content)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def sendEmail(address, subject, content):
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.starttls()
+    server.login('jgtmadushan@gmail.com', 'Jg8612036@')
+    email = EmailMessage()
+    email['From'] = 'jgtmadushan@gmail.com'
+    email['To'] = address
+    email['Subject'] = subject
+    email.set_content(content)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    server.send_message(email)
+
+get_mail_info()
